a10386d2e5303aa1f6c0e7c3f85314093f80f6b0
Sergei Pavlov <gsergius@mail.ru>
2022-05-18 19:14:53 +0300

    stake tests

--- test/stake-test.ts
+++ test/stake-test.ts
@@ -1,12 +1,12 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { BigNumber } from "ethers";
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import { provideLiquidityForTests } from "../scripts/provide-liquidity";
 import { IERC20, StakingPlatform } from "../typechain-types";
 import { testDeployment } from "./test-deployment";
 
-describe("stake functions", () => {
+describe("stake", () => {
     let accounts: SignerWithAddress[];
     let owner: SignerWithAddress;
     let rewardTokenOwner: SignerWithAddress;
@@ -34,13 +34,92 @@ describe("stake functions", () => {
             .mint(contract.address, 50000);
     });
 
-    it("should work", async () => {
-        await stakingToken.approve(contract.address, availableLpTokenBalance);
+    it("should increase staked amount", async () => {
+        const halfOfAmount = Math.floor(availableLpTokenBalance.toNumber() / 2);
+        await stakingToken.approve(contract.address, 2 * halfOfAmount);
 
-        await contract.stake(availableLpTokenBalance);
+        //stake #1
+        await contract.stake(halfOfAmount);
+        let [stakedAmount, , ,] = await contract.getDetails(staker.address);
+        expect(stakedAmount).eq(halfOfAmount);
 
-        const [stakedAmount,,,] = 
-            await contract.getDetails(staker.address);
-        expect(stakedAmount).eq(availableLpTokenBalance);
+        //stake #2
+        await contract.stake(halfOfAmount);
+        [stakedAmount, , ,] = await contract.getDetails(staker.address);
+        expect(stakedAmount).eq(2 * halfOfAmount);
     });
+
+    it("should change dates", async () => {
+        const halfOfAmount = Math.floor(availableLpTokenBalance.toNumber() / 2);
+        await stakingToken.approve(contract.address, 2 * halfOfAmount);
+
+        //stake #1
+        await contract.stake(halfOfAmount);
+        let [, , lastStakeDate1, lastRewardDate1] = 
+            await contract.getDetails(staker.address);
+        //stake #2
+        await contract.stake(halfOfAmount);
+        let [, , lastStakeDate2, lastRewardDate2] =
+            await contract.getDetails(staker.address);
+
+        expect(toDate(lastStakeDate2)).greaterThan(toDate(lastStakeDate1));
+        expect(toDate(lastRewardDate2)).greaterThan(toDate(lastRewardDate1));
+    });
+
+    it("should accumulate rewards", async () => {
+        const oneThird = Math.floor(availableLpTokenBalance.toNumber() / 3);
+        await stakingToken.approve(contract.address, 3 * oneThird);
+
+        //stake #1
+        await contract.stake(oneThird);
+        
+        const rewardPercentage = await contract.getRewardPercentage();
+        const rewardDelay = await contract.getRewardDelay();
+        await network.provider.send(
+            "evm_increaseTime", 
+            [rewardDelay.toNumber()]
+        );
+        let expectedReward = 
+            Math.floor(oneThird * rewardPercentage.toNumber() / 100);
+
+        //stake #2
+        await contract.stake(oneThird);
+        let [, actualReward, ,] = await contract.getDetails(staker.address);
+        expect(actualReward.toNumber()).eq(expectedReward);
+
+        expectedReward +=
+            Math.floor(2*oneThird * rewardPercentage.toNumber() / 100);
+
+        await network.provider.send(
+            "evm_increaseTime",
+            [rewardDelay.toNumber()]
+        );
+
+        //stake #3
+        await contract.stake(oneThird);
+        [, actualReward, ,] = await contract.getDetails(staker.address);
+        expect(actualReward.toNumber()).eq(expectedReward);
+    });
+
+    it("shouldn't calculate reward if too early", async () => {
+        const oneThird = Math.floor(availableLpTokenBalance.toNumber() / 3);
+        await stakingToken.approve(contract.address, 3 * oneThird);
+
+        //stake #1
+        await contract.stake(oneThird);
+
+        const rewardDelay = await contract.getRewardDelay();
+        await network.provider.send(
+            "evm_increaseTime",
+            [rewardDelay.toNumber() - 60*1000]//-60 seconds
+        );
+
+        const expectedReward = 0;
+        //stake #2
+        await contract.stake(oneThird);
+        let [, actualReward, ,] = await contract.getDetails(staker.address);
+        expect(actualReward.toNumber()).eq(expectedReward);
+    });
+
+    function toDate(i: BigNumber) { return new Date(i.toNumber() * 1000); }
 });
