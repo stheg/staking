6d251172dd60632d631689b8a1340e9c9b3def8b
Sergei Pavlov <gsergius@mail.ru>
2022-05-20 19:48:09 +0300

    get-details task

--- tasks/staking.ts
+++ tasks/staking.ts
@@ -68,12 +68,37 @@ task("claim", "Allows to withdraw available reward tokens")
             staker
         ) as IMintableERC20;
 
-        // just for test
-        await rewardToken.connect(ercOwner)
-            .mint(stakingPlatform.address, 5000);
+        if (hre.network.name == "hardhat") {
+            // just for test
+            await rewardToken.connect(ercOwner)
+                .mint(stakingPlatform.address, 5000);
+        }
 
+        let r = await rewardToken.balanceOf(staker.address);
+        console.log("Amount of reward tokens before claim: " + r);
         await stakingPlatform.claim();
 
-        const res = await rewardToken.balanceOf(staker.address);
-        console.log("Current amount of reward tokens: " + res);
+        r = await rewardToken.balanceOf(staker.address);
+        console.log("Current amount of reward tokens: " + r);
     });
+
+task("get-details", "Returns the details")
+    .addParam("contract", "address of the staking platform")
+    .addOptionalParam("staker", "address of the account")
+    .setAction(async (args, hre) => {
+        let [contractOwner, ercOwner, staker] = await hre.ethers.getSigners();
+        if (args.staker)
+            staker = await hre.ethers.getSigner(args.staker);
+
+        const stakingPlatform = await hre.ethers.getContractAt(
+            "StakingPlatform",
+            args.contract,
+            staker
+        ) as StakingPlatform;
+
+        const [a, r, ud, rd] = await stakingPlatform.getDetails(staker.address);
+        console.log("Amount: " + a);
+        console.log("Reward: " + r);
+        console.log("Unstake Date: " + ud);
+        console.log("Reward Date: " + rd);
+    }); 
